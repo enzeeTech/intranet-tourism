e3f42bf554c8e636989b0d59f7917b8b76de693c
Freezeyy <fareezhishamuddin@gmail.com>
2024-04-04 11:43:39 +0800

    hover-event

--- resources/js/Pages/Calendar/Calendar.jsx
+++ resources/js/Pages/Calendar/Calendar.jsx
@@ -1,27 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
 import interactionPlugin from '@fullcalendar/interaction';
-
+import * as bootstrap from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 function Calendar() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleDateSelect = (info) => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div>
-        <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            initialView={"dayGridMonth"}
-            headerToolbar={
-                {
-                    start: 'prev,next today', 
-                    center: 'title',
-                    end: 'dayGridMonth,timeGridWeek,timeGridDay' 
-                }
-            }
-            height={'100vh'}
-        />
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
+        initialView="dayGridMonth"
+        selectable={true}
+        selectHelper={true}
+        select={handleDateSelect}
+        headerToolbar={{
+          start: 'prev,next today',
+          center: 'title',
+          end: 'dayGridMonth,timeGridWeek,timeGridDay,list',
+        }}
+        height={'100vh'}
+        buttonText={{
+          today: 'Today',
+          month: 'Month',
+          week: 'Week',
+          day: 'Day',
+        }}
+        events={[
+          {
+            title: 'Standup Meeting Tourism',
+            start: '2024-04-04T11:00',
+            end: '2024-04-04T12:00',
+            color: 'black',
+          },
+        ]}
+        eventDidMount={(info) => {
+          return new bootstrap.Popover(info.el, {
+            title: info.event.title,
+            placement: "auto",
+            trigger: "hover",
+            customClass: "popoverStyle",
+            content:
+              "<p>Come to Oval Room</p>",
+            html: true,
+          });
+        }}
+      />
+
+      {isModalOpen && (
+        <div
+          style={{
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: 'white',
+            border: '1px solid #ccc',
+            padding: '20px',
+            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+            zIndex: '1050',
+          }}
+        >
+          <button
+            onClick={closeModal}
+            style={{
+              position: 'absolute',
+              top: '10px',
+              right: '10px',
+              border: 'none',
+              background: 'none',
+              cursor: 'pointer',
+            }}
+          >
+            X
+          </button>
+          <h4>Create Event</h4>
+          <input type="text" className="form-control" />
+          <button>Confirm</button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Calendar
+export default Calendar;
