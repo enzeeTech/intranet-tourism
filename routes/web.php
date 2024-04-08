b9f638350f05cd5bb524b39db50ad9c03c287ddc
Benjamin Irfan <benjaminirfan.work@gmail.com>
2024-04-08 16:00:39 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
+        //'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
 });
