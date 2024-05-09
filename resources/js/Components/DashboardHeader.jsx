77c5a4c2cbfa7a209fbc76333bfe653440583f95
Dilshan Jayasinghe <dilshan.sj@socio.com.my>
2024-05-10 01:50:41 +0800

    Fixed dashboard header styling

--- resources/js/Components/DashboardHeader.jsx
+++ resources/js/Components/DashboardHeader.jsx
@@ -12,19 +12,17 @@ import NotificationPopup from '../Components/Noti-popup-test';
   
 const Header = () => {
   return (
-    <header className="flex items-center justify-between p-4 bg-white shadow-md sticky top-0 z-50">
-
-
-       <a href= '../dashboard'><img src={logo}   alt="Logo" className="h-12 ml-8" /> </a>
-      <div className="relative flex items-center flex-grow mx-8">
-        <img src={search} alt="Search" className="absolute w-6 h-6 transform -translate-y-1/2 left-3 top-1/2" />
+    <header className="sticky top-0 z-50 flex items-center justify-between p-4 bg-white shadow-md">
+      <a href= '../dashboard'><img src={logo}   alt="Logo" className="h-12 ml-8" /> </a>
+      <div className="relative flex items-center flex-grow mx-8 ">
+      <img src={search} alt="Search" className="absolute w-6 h-6 transform -translate-y-1/2 left-3 top-1/2" />
         <input
           type="text"
           placeholder="Search..."
           className="search-input"
-          style={{ width: '1000px', paddingLeft: '3rem', borderColor: 'black', borderRadius: '2rem', transition: 'border-color 0.3s' }}
+          style={{ width: '1000px', paddingLeft: '3rem', borderColor: 'transparent', borderRadius: '2rem',  }}
           onFocus={(e) => { e.target.style.borderColor = 'blue'; }}
-          onBlur={(e) => { e.target.style.borderColor = 'black'; }}
+          onBlur={(e) => { e.target.style.borderColor = 'transparent'; }}
         />
       </div>
 
@@ -32,9 +30,9 @@ const Header = () => {
 
         <Menu as="div" className="relative inline-block text-left">
           <div >
-            <Menu.Button className="focus:outline-none hover:underline relative ">
+            <Menu.Button className="relative focus:outline-none hover:underline ">
             <img src= "/assets/notification.png" alt="Notification" className="w-5 h-6 mr-1" />
-              <div className="absolute top-0 left-0 w-10 h-10 opacity-0 transition duration-300 ease-in-out rounded-sm ">
+              <div className="absolute top-0 left-0 w-10 h-10 transition duration-300 ease-in-out rounded-sm opacity-0 ">
 
 
               </div>
@@ -49,7 +47,7 @@ const Header = () => {
             leaveFrom="transform opacity-100 scale-100"
             leaveTo="transform opacity-0 scale-95"
           >
-            <Menu.Items className="absolute right-0 z-10 mt-2 w-96  max-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-96 max-full ring-1 ring-black ring-opacity-5 focus:outline-none">
               <div className="">
              
                 <Menu.Items>
@@ -57,9 +55,9 @@ const Header = () => {
                 </Menu.Items>
 
             <div className="flex flex-row w-full h-10 hover:bg-gray-200 ">  
-            <a href="http://127.0.0.1:8000/notification" className="flex items-center w-full bg-white-500  text-black font-bold py-2 px-4 rounded">
+            <a href="http://127.0.0.1:8000/notification" className="flex items-center w-full px-4 py-2 font-bold text-black rounded bg-white-500">
               View All
-              <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-4 w-6 ml-4">
+              <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-6 h-4 ml-4">
                 <path d="M1 6H8" stroke="#222222" strokeLinecap="round"/>
                 <path d="M11.7941 5.84152C11.8981 5.92158 11.8981 6.07842 11.7941 6.15848L10.072 7.48418C9.94049 7.58542 9.75 7.49166 9.75 7.32569L9.75 4.6743C9.75 4.50834 9.94049 4.41458 10.072 4.51582L11.7941 5.84152Z" fill="#222222"/>
                 <path fillRule="evenodd" clipRule="evenodd" d="M15 6C15 8.76142 12.7614 11 9.99999 11C8.46035 11 7.08325 10.3041 6.16606 9.2097C6.05778 9.08051 5.90044 9 5.73188 9C5.31911 9 5.069 9.44402 5.32822 9.76525C6.42809 11.1282 8.11223 12 9.99999 12C13.3137 12 16 9.31371 16 6C16 2.68629 13.3137 0 9.99999 0C8.11223 0 6.42809 0.871801 5.32822 2.23475C5.069 2.55598 5.31911 3 5.73188 3C5.90044 3 6.05779 2.91949 6.16606 2.7903C7.08325 1.69589 8.46035 1 9.99999 1C12.7614 1 15 3.23858 15 6Z" fill="#222222"/>
@@ -70,9 +68,9 @@ const Header = () => {
 
                 {/*TESST*/}
             {/* <div className="flex flex-row w-full h-10 ">
-              <a href="http://127.0.0.1:8000/notipopup" className=" flex items-center bg-white-500 hover:bg-gray-200 text-black font-bold py-2 px-4 rounded ">      
+              <a href="http://127.0.0.1:8000/notipopup" className="flex items-center px-4 py-2 font-bold text-black rounded bg-white-500 hover:bg-gray-200">      
 
-            TEST<img src={"http://127.0.0.1:5173/public/assets/view-all-icon.png"} alt="Additional Image" className=" h-4 w-6 ml-4" />
+            TEST<img src={"http://127.0.0.1:5173/public/assets/view-all-icon.png"} alt="Additional Image" className="w-6 h-4 ml-4 " />
 
 
               </a>
