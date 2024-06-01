d689e235eb08afb579cd6afeab64f3cf4cbb6031
Mohamad Safwan <mohamadsafwan.1905@gmail.com>
2024-06-01 13:28:13 +0800

    [feat] update

--- resources/js/Layouts/DashboardLayoutNew.jsx
+++ resources/js/Layouts/DashboardLayoutNew.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 import Header from '../Components/DashboardHeaderNew';
 import Sidebar from '../Components/SideNavBarNew';
 import { useState } from 'react';
-import PageTitle from '../Components/Reusable/PageTitle';
-import FeaturedEvents from '../Components/Reusable/FeaturedEventsWidget/FeaturedEvents';
-import WhosOnline from '../Components/Reusable/WhosOnlineWidget/WhosOnline';
-import SearchFile from '../Components/Reusable/FileManagementSearchBar';
-import { FileTable } from '@/Components/FileManagement';
 import '../Components/Reusable/css/FileManagementSearchBar.css';
-import Layout from '@/Layouts/DashboardLayoutNew';
 
 
 
