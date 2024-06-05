519bbaf682165894e06f72e6113df481702b55c7
Benjamin Irfan <benjaminirfan.work@gmail.com>
2024-06-05 21:25:54 +0800

    committing to the new version

--- resources/js/Components/Reusable/WallPosting/InputBox.jsx
+++ resources/js/Components/Reusable/WallPosting/InputBox.jsx
@@ -1,33 +1,22 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-//import searchIcon from '../../../../public/assets/searchStaffButton.png';
-//import './css/FileManagementSearchBar.css';
-//import './css/General.css';
+import React, { useState, useRef } from "react";
+import { Polls } from "./InputPolls";
+import { People } from "./InputPeople";
+import '../css/InputBox.css';
+import '../../../Pages/Calendar/index.css';
 
-const SearchFile = ({ onSearch, requiredData }) => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [file, setFile] = useState(null);
+function ShareYourThoughts() {
+  const [inputValue, setInputValue] = useState("");
+  const [showPollPopup, setShowPollPopup] = useState(false);
+  const [showPeoplePopup, setShowPeoplePopup] = useState(false);
+  const [attachments, setAttachments] = useState([]);
+  const [fileNames, setFileNames] = useState([]);
+  const textAreaRef = useRef(null);
 
-  const handleSearch = () => {
-    console.log(searchTerm);
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
   };
 
-  const handleFileChange = (event) => {
-    const selectedFile = event.target.files[0];
-    setFile(selectedFile);
-  };
-
-  const handleFileUpload = async () => {
-    if (!file) {
-      console.log('No file selected.');
-      return;
-    }
-
-    if (!requiredData) {
-      console.log('Required data is not available.');
-      return;
-    }
-
+  const handleClickSend = () => {
     const formData = new FormData();
     formData.append('user_id', '1');
     formData.append('type', 'post');
@@ -86,53 +75,100 @@ const SearchFile = ({ onSearch, requiredData }) => {
     setShowPollPopup(true);
   };
 
-  const handleFileDelete = () => {
-    setFile(null);
+  const handleClickPeople = () => {
+    setShowPeoplePopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPollPopup(false);
+    setShowPeoplePopup(false);
   };
 
   return (
-    <div className="file-search-bar-container">
-      <div className="file-search-bar-title">
-        <h2>Search Files...</h2>
-      </div>
-      <div className="file-search-bar">
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Search Name"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          style={{ paddingLeft: '1.2rem' }}
-        />
-        <button onClick={handleSearch}>
-          <img src="assets/filesearchbutton.svg" alt="filesearchbutton" className="" />
-        </button>
-        {file && (
-          <div className="file-preview">
-            <span>{file.name}</span>
-            <button onClick={handleFileDelete}>x</button>
-          </div>
-        )}
-        {!file && (
-          <label htmlFor="file-upload">
+    <section className="flex flex-col justify-center text-sm max-w-[610px] text-neutral-800">
+      <div className="input-box-container mt-16 flex gap-5 justify-between px-8 pt-5 pb-2 bg-white rounded-2xl shadow-sm max-md:flex-wrap max-md:px-5 max-md:max-w-full">
+        <div className="flex flex-col w-full">
+          <textarea
+            ref={textAreaRef}
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="Share Your Thoughts..."
+            className="self-center mt-1 h-8 px-2 text-sm border-none appearance-none resize-none input-no-outline"
+          />
+            <div className="flex mt-7 items-center">
+                <div className="flex gap-3">
+                <button>
+                    <img
+                    loading="lazy"
+                    src="assets/inputpolls.svg"
+                    alt="Icon 1"
+                    className="w-[15px] h-auto"
+                    onClick={handleClickPoll}
+                    />
+                </button>
+                <button>
+                    <img
+                    loading="lazy"
+                    src="assets/inputimg.svg"
+                    alt="Icon 2"
+                    className="w-[15px] h-auto"
+                    onClick={handleClickImg}
+                    />
+                </button>
+                <button>
+                    <img
+                    loading="lazy"
+                    src="assets/inputvid.svg"
+                    alt="Icon 3"
+                    className="w-[15px] h-auto"
+                    onClick={handleClickVid}
+                    />
+                </button>
+                <button>
+                    <img
+                    loading="lazy"
+                    src="assets/inputdoc.svg"
+                    alt="Icon 4"
+                    className="w-[15px] h-auto"
+                    onClick={handleClickDoc}
+                    />
+                </button>
+                <button>
+                    <img
+                    loading="lazy"
+                    src="assets/inputpeople.svg"
+                    alt="Icon 5"
+                    className="w-[10px] h-auto"
+                    onClick={handleClickPeople}
+                    />
+                </button>
+                </div>
+                <div className="file-names-container flex flex-wrap gap-2" style={{ minWidth: `${fileNames.length * 80}px` }}>
+                    {fileNames.map((name, index) => (
+                        <div className="flex items-center px-2 py-1 bg-white rounded-lg shadow" key={index}>
+                            <div className="file-name text-xs truncate">{name}</div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+        <button
+          onClick={handleClickSend}
+          className="send-button"
+        >
+          <div className="send-button-icon">
             <img
               loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/09e8f0029fa709f52ac7d218876a28da6904c7ef7108cbb12df1fb413678c59c?apiKey=285d536833cc4168a8fbec258311d77b&"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/bb9e6a4fb4fdc3ecfcef04a0984faf7c2720a004081fccbe4db40b1509a23780?apiKey=23ce5a6ac4d345ebaa82bd6c33505deb&"
               alt=""
-              className="shrink-0 my-auto aspect-[1.45] w-[49px]"
             />
-          </label>
-        )}
-        <input
-          id="file-upload"
-          type="file"
-          onChange={handleFileChange}
-          style={{ display: 'none' }}
-        />
-        <button onClick={handleFileUpload}>Upload File</button>
+          </div>
+        </button>
       </div>
-    </div>
+      {showPollPopup && <Polls onClose={closePopup} />}
+      {showPeoplePopup && <People onClose={closePopup} />}
+    </section>
   );
-};
+}
 
-export default SearchFile;
+export default ShareYourThoughts;
