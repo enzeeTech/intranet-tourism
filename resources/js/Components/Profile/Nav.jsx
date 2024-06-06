1027cd2a26b2f24a2e709c3d86e907673af9eadc
Mohamad Safwan <mohamadsafwan.1905@gmail.com>
2024-06-06 20:28:19 +0800

    [feat] new update

--- resources/js/Components/Profile/Nav.jsx
+++ resources/js/Components/Profile/Nav.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 
 function ProfileNav({ activeTab, setActiveTab }) {
+    const tabs = [
+        { name: 'Activities', key: 'activities' },
+        { name: 'Bio', key: 'bio' },
+        { name: 'Gallery', key: 'gallery' },
+        { name: 'Files', key: 'files' }
+    ];
+
     return (
-      <nav className="flex gap-5 justify-between mt-14 ml-28 mr-28 max-w-full text-sm font-semibold text-center whitespace-nowrap text-neutral-800 text-opacity-30 w-auto max-md:flex-wrap max-md:mt-10">
-        <a href="#activities" className={`text-stone-300 ${activeTab === "activities" ? "font-bold text-blue-500" : ""}`} onClick={() => setActiveTab("activities")}>Activities</a>
-        <a href="#bio" className={`text-stone-300 ${activeTab === "bio" ? "text-blue-500" : ""}`} onClick={() => setActiveTab("bio")}>Bio</a>
-        <a href="#gallery" className={`text-stone-300 ${activeTab === "gallery" ? "font-bold text-blue-500" : ""}`} onClick={() => setActiveTab("gallery")}>Gallery</a>
-        <a href="#files" className={`text-stone-300 ${activeTab === "files" ? "font-bold text-blue-500" : ""}`} onClick={() => setActiveTab("files")}>Files</a>
-      </nav>
+        <nav className="flex gap-5 justify-between mt-14 ml-28 mr-28 max-w-full text-sm font-semibold text-center whitespace-nowrap text-neutral-800 text-opacity-30 w-auto max-md:flex-wrap max-md:mt-10">
+            {tabs.map(tab => (
+                <span
+                    key={tab.key}
+                    className={`cursor-pointer ${activeTab === tab.key ? "font-bold text-blue-500" : "text-stone-300"}`}
+                    onClick={() => setActiveTab(tab.key)}
+                >
+                    {tab.name}
+                </span>
+            ))}
+        </nav>
     );
 }
 
 export default ProfileNav;
-
