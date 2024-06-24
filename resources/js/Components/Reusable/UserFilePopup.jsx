d4677a96aa3e457acc579b1fd09278697fa05472
Zahrin Azam <kidzenfxc@gmail.com>
2024-06-24 22:12:46 +0800

    bugfix

--- resources/js/Components/Reusable/UserFilePopup.jsx
+++ resources/js/Components/Reusable/UserFilePopup.jsx
@@ -225,10 +225,6 @@ export default PopupContent;
 //             )}
 //         </div>
 //     );
-<<<<<<< HEAD
-=======
-
->>>>>>> cff323d985c51d062207df36907acee8020e16ad
 // };
 
 // export default UserFilePopup;
