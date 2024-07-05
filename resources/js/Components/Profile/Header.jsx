7f69345113bb4f254bea2c9cab31d413d96bf765
faridizzuddin101 <faridizzuddin101@gmail.com>
2024-07-05 15:30:38 +0800

    Fix Profile Icon responsiveness for mobile view in My Profile

--- resources/js/Components/Profile/Header.jsx
+++ resources/js/Components/Profile/Header.jsx
@@ -73,14 +73,14 @@ import './profile.css'
 
 function ProfileImage({ src, alt, className, rounded }) {
   return (
-    <div className={`flex overflow-hidden relative z-10 flex-col items-end px-16 pt-20 pb-3.5 mt-24 mb-0 w-44 max-w-full aspect-square max-md:px-5 max-md:mt-10 max-md:mb-2.5 ${className}`}>
+    <div className={`flex overflow-hidden relative z-10 flex-col items-end px-16 pt-20 pb-3.5 mt-24 mb-0 w-44 max-w-full aspect-square max-md:px-5 max-md:mt-10 max-md:mb-2.5 ${className} max-md:w-32 max-md:mt-10`}>
       <img
         src={src}
         alt={alt}
-        className={`object-cover absolute inset-0 bottom-5 top-0 size-[158px] mb-12 ${rounded ? 'rounded-full' : ''}`}
+        className={`object-cover absolute inset-0 bottom-5 top-0 size-[158px] mb-12 ${rounded ? 'rounded-full' : ''} max-md:w-32 max-md:h-32 max-md:bottom-0`} 
       />
       <div className="relative w-full h-0 flex justify-end mt-16 max-md:mt-10">
-        <div className="w-5 h-5 bg-green-500 rounded-full border-2 border-white border-solid stroke-[2px] absolute bottom-0.5 right-0 left-14" />
+        <div className="absolute w-5 h-5 bg-green-500 rounded-full border-2 border-white border-solid stroke-[2px] bottom-0.5 right-0 left-14 max-md:left-auto max-md:right-0 max-md:bottom-0" />
       </div>
     </div>
   );
@@ -117,7 +117,7 @@ function ProfileHeader({ backgroundImage, profileImage, name, status, onEditBann
       className="flex overflow-hidden relative z-999 flex-col items-start px-7 pt-32 -mt-14 w-full min-h-[400px] max-md:px-5 max-md:max-w-full"
       onClick={handleEditBanner}
     >
-      <img src={backgroundImage} alt="" className="object-cover absolute inset-0 w-full h-4/5 rounded-lg shadow-custom" />
+      <img src={backgroundImage} alt="" className="object-cover absolute inset-0 w-full h-4/5 max-md:h-3/5 rounded-lg shadow-custom" />
       <div onClick={handleIconClick}>
         <ProfileImage src={profileImage} alt={`${name}'s profile picture`} rounded={rounded} />
         {isPopupOpen && (
@@ -131,9 +131,9 @@ function ProfileHeader({ backgroundImage, profileImage, name, status, onEditBann
           />
         )}
       </div>
-      <div className="flex flex-col self-start px-5 -mt-16 -ml-10 max-md:-ml-5 max-md:mt-5">
-        <h1 className="self-end text-3xl font-extrabold text-neutral-800 relative ml-48 max-md:text-xl max-md:ml-0">{name}</h1>
-        <div className="mt-2 text-xs font-semibold text-neutral-800 text-opacity-50 ml-48 max-md:text-sm max-md:ml-0">{status}</div>
+      <div className="flex flex-col self-start px-5 -mt-16 -ml-10 max-md:-ml-3 max-md:mt-5">
+        <h1 className="self-end text-3xl font-extrabold text-neutral-800 relative ml-48 max-md:text-xl max-md:ml-5 max-sm:ml-7">{name}</h1>
+        <div className="mt-2 text-xs font-semibold text-neutral-800 text-opacity-50 ml-48 max-md:text-sm max-md:ml-5 max-sm:ml-7">{status}</div>
       </div>
     </header>
   );
