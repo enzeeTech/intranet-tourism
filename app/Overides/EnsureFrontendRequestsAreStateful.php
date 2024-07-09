1ef197dc01a864637c96cd632425bc5969f003e0
Zahrin Azam <kidzenfxc@gmail.com>
2024-07-09 15:20:56 +0800

    bugfix: api doc statefull

---
+++ app/Overides/EnsureFrontendRequestsAreStateful.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Overides;
+
+use Illuminate\Pipeline\Pipeline;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful as BaseEnsureFrontendRequestsAreStateful;
+
+class EnsureFrontendRequestsAreStateful extends BaseEnsureFrontendRequestsAreStateful
+{
+
+    /**
+     * Determine if the given request is from the first-party application frontend.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    public static function fromFrontend($request)
+    {
+        $domain = $request->headers->get('referer') ?: $request->headers->get('origin');
+
+        if (is_null($domain) || Str::contains($domain, 'docs/api')) {
+            return false;
+        }
+
+        $domain = Str::replaceFirst('https://', '', $domain);
+        $domain = Str::replaceFirst('http://', '', $domain);
+        $domain = Str::endsWith($domain, '/') ? $domain : "{$domain}/";
+
+        $stateful = array_filter(config('sanctum.stateful', []));
+
+        return Str::is(Collection::make($stateful)->map(function ($uri) {
+            return trim($uri) . '/*';
+        })->all(), $domain);
+    }
+}
