407b183f5a4c2ed61f919ad14a07a035da5f0706
Mohamad Safwan <mohamadsafwan.1905@gmail.com>
2024-07-11 16:48:36 +0800

    fix pautan

--- resources/js/Components/Settings/Pautan.jsx
+++ resources/js/Components/Settings/Pautan.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
+import { ArrowLongLeftIcon, ArrowLongRightIcon } from '@heroicons/react/20/solid';
+import './Pautan.css';
 
 const API_URL = '/api/settings/external_links';
 const urlTemplate = '/api/settings/external_links/{id}';
+const PAGE_SIZE = 15;
 
 const Pautan = () => {
   const [apps, setApps] = useState([]);
@@ -12,17 +15,41 @@ const Pautan = () => {
   const [currentApp, setCurrentApp] = useState(null);
   const [newAppName, setNewAppName] = useState('');
   const [newAppUrl, setNewAppUrl] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    fetch(API_URL)
-      .then(response => response.json())
-      .then(responseData => {
-        const appsData = responseData.data?.data || [];
-        const sortedAppsData = appsData.sort((a, b) => a.order - b.order);
+    const fetchData = async () => {
+      let allApps = [];
+      let currentPage = 1;
+      let lastPage = 1;
+
+      try {
+        while (currentPage <= lastPage) {
+          const response = await fetch(`${API_URL}?page=${currentPage}`, {
+            method: "GET",
+            headers: { Accept: 'application/json' }
+          });
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const data = await response.json();
+          allApps = allApps.concat(data.data.data);
+          lastPage = data.data.last_page;
+          currentPage++;
+        }
+        const sortedAppsData = allApps.sort((a, b) => a.order - b.order);
         setApps(sortedAppsData);
-      })
-      .catch(error => console.error('Error fetching data:', error));
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
   }, []);
+
+  const sortAlphabetically = (apps) => {
+    return apps.sort((a, b) => a.label.localeCompare(b.label));
+  };
 
   const updateOrder = (newApps) => {
     const updatedApps = newApps.map((app, idx) => ({
@@ -30,7 +57,7 @@ const Pautan = () => {
       order: idx + 1
     }));
 
-    setApps(updatedApps);
+    setApps(sortAlphabetically(updatedApps));
 
     const updatePromises = updatedApps.map((app) => {
       const updateUrl = urlTemplate.replace('{id}', app.id);
@@ -38,9 +65,7 @@ const Pautan = () => {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(app)
-      })
-        .then(response => response.ok ? response.json() : response.text().then(errorText => { throw new Error(`Server error: ${errorText}`); }))
-        .catch(error => console.error(`Error updating app with id ${app.id}:`, error.message));
+      }).catch(error => console.error(`Error updating app with id ${app.id}:`, error.message));
     });
 
     Promise.all(updatePromises)
@@ -74,25 +99,24 @@ const Pautan = () => {
     updateOrder(newApps);
   };
 
-    const PautanHandleAddApp = () => {
-        const newApp = { label: newAppName, url: newAppUrl };
+  const PautanHandleAddApp = () => {
+    const newApp = { label: newAppName, url: newAppUrl };
 
-        fetch(API_URL, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(newApp)
-        })
-        .then(response => response.json())
-        .then(data => {
-            setApps([...apps, data]);
-            setNewAppName('');
-            setNewAppUrl('');
-            setIsAddModalVisible(false);
-        })
-        // .catch(error => console.error('Error adding app:', error));
-        window.location.reload();
-    };
-
+    fetch(API_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newApp)
+    })
+      .then(response => response.json())
+      .then(data => {
+        setApps(sortAlphabetically([...apps, data]));
+        setNewAppName('');
+        setNewAppUrl('');
+        setIsAddModalVisible(false);
+      })
+      // .catch(error => console.error('Error adding app:', error));
+      window.location.reload();
+  };
 
   const PautanHandleEditApp = (app) => {
     setCurrentApp(app);
@@ -112,7 +136,7 @@ const Pautan = () => {
     })
       .then(response => response.json())
       .then(data => {
-        setApps(apps.map(app => (app.id === data.id ? data : app)));
+        setApps(sortAlphabetically(apps.map(app => (app.id === data.id ? data : app))));
         setCurrentApp(null);
         setNewAppName('');
         setNewAppUrl('');
@@ -130,7 +154,7 @@ const Pautan = () => {
     })
       .then(response => {
         if (response.status === 204) {
-          setApps(apps.filter(app => app.id !== currentApp.id));
+          setApps(sortAlphabetically(apps.filter(app => app.id !== currentApp.id)));
           setIsDeleteModalVisible(false);
           setCurrentApp(null);
         } else {
@@ -140,15 +164,19 @@ const Pautan = () => {
       .catch(error => console.error('Error deleting app:', error));
   };
 
-  const handleSave = () => {
-    console.log("Changes saved", apps);
+  const getCurrentPageApps = () => {
+    const start = (currentPage - 1) * PAGE_SIZE;
+    const end = start + PAGE_SIZE;
+    return apps.slice(start, end);
   };
+
+  const totalPages = Math.ceil(apps.length / PAGE_SIZE);
 
   return (
     <>
       <section className="flex flex-col px-5 py-4 bg-white rounded-2xl shadow-custom max-w-[900px] mb-10">
         <div className="flex items-center justify-between mb-2 border-b border-gray-200">
-          <h2 className="mb-3 text-2xl font-bold text-blue-500 ">External Apps</h2>
+          <h2 className="mb-3 text-2xl font-bold text-blue-500">External Apps</h2>
           <button className="px-4 py-1 mb-2 font-bold text-white bg-blue-500 rounded-full" onClick={() => setIsAddModalVisible(true)}>+ Add</button>
         </div>
         <DragDropContext onDragEnd={PautanHandleDragEnd}>
@@ -157,45 +185,54 @@ const Pautan = () => {
               <table className="min-w-full divide-y divide-gray-200" {...provided.droppableProps} ref={provided.innerRef}>
                 <thead>
                   <tr>
-                    <th className="px-6 py-3 text-base font-bold text-center text-gray-900">App name</th>
-                    <th className="px-6 py-3 text-base font-bold text-center text-gray-900">URL</th>
-                    <th className="px-6 py-3 text-base font-bold text-center text-gray-900">Order</th>
+                    <th className="px-6 py-3 text-base font-bold text-center text-gray-900 label-column">App name</th>
+                    <th className="px-6 py-3 text-base font-bold text-center text-gray-900 url-column">URL</th>
+                    <th className="px-6 py-3 text-base font-bold text-center text-gray-900 order-column">Order</th>
                     <th className="px-6 py-3 text-base font-bold text-center text-gray-900">Edit</th>
                     <th className="px-6 py-3 text-base font-bold text-center text-gray-900">Delete</th>
                   </tr>
                 </thead>
                 <tbody>
-                  {apps.map((app, index) => (
+                  {getCurrentPageApps().map((app, index) => (
                     <Draggable key={app.id} draggableId={String(app.id)} index={index}>
-                      {(provided) => (
-                        <tr ref={provided.innerRef} className="bg-white border-t border-gray-200" {...provided.draggableProps}>
-                          <td className="px-6 py-4 text-sm font-semibold text-black whitespace-nowrap" {...provided.dragHandleProps}>
+                      {(provided, snapshot) => (
+                        <tr
+                          ref={provided.innerRef}
+                          className={`bg-white border-t border-gray-200 ${snapshot.isDragging ? 'dragging' : ''}`}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <td className="px-6 py-4 text-sm font-semibold text-black whitespace-nowrap label-column">
                             <input
                               type="text"
+                              disabled={true}
                               value={app.label}
+                              readOnly
                               onChange={(e) => {
                                 const newApps = [...apps];
                                 newApps[index].label = e.target.value;
                                 setApps(newApps);
                               }}
-                              className="w-full p-1 border rounded-md outline-none border-E4E4E4"
+                              className="w-full p-1 outline-none border-none"
                               style={{ borderColor: '#E4E4E4', borderRadius: '0.375rem', borderWidth: '1px' }}
                             />
                           </td>
-                          <td className="px-6 py-4 text-sm font-semibold text-black whitespace-nowrap">
+                          <td className="px-6 py-4 text-sm font-semibold text-black whitespace-nowrap url-column">
                             <input
                               type="text"
+                              disabled={true}
                               value={app.url}
+                              readOnly
                               onChange={(e) => {
                                 const newApps = [...apps];
                                 newApps[index].url = e.target.value;
                                 setApps(newApps);
                               }}
-                              className="w-full p-1 border rounded-md outline-none border-E4E4E4"
+                              className="w-full p-1 outline-none border-none"
                               style={{ borderColor: '#E4E4E4', borderRadius: '0.375rem', borderWidth: '1px' }}
                             />
                           </td>
-                          <td className="px-6 py-4 text-sm font-semibold text-black whitespace-nowrap">
+                          <td className="px-6 py-4 text-sm font-semibold text-black whitespace-nowrap order-column">
                             <div className="flex items-center justify-center">
                               <button
                                 className="px-2"
@@ -223,15 +260,15 @@ const Pautan = () => {
                           </td>
                           <td className="px-6 py-4 text-sm font-semibold text-black whitespace-nowrap">
                             <div className="flex items-center justify-center">
-                              <button className="text-blue-500" onClick={(e) => { e.stopPropagation(); PautanHandleEditApp(app); }}>
-                                <img src="assets/EditIcon.svg" alt="Edit" />
+                              <button className="text-blue-100" onClick={(e) => { e.stopPropagation(); PautanHandleEditApp(app); }}>
+                                <img className="w-7" src="assets/EditIcon.svg" alt="Edit" />
                               </button>
                             </div>
                           </td>
                           <td className="px-6 py-4 text-sm font-semibold text-black whitespace-nowrap">
                             <div className="flex items-center justify-center">
                               <button className="text-red-500" onClick={(e) => { e.stopPropagation(); setCurrentApp(app); setIsDeleteModalVisible(true); }}>
-                                <img src="assets/redDeleteIcon.svg" alt="Delete" />
+                                <img className="w-7" src="assets/redDeleteIcon.svg" alt="Delete" />
                               </button>
                             </div>
                           </td>
@@ -246,6 +283,42 @@ const Pautan = () => {
           </Droppable>
         </DragDropContext>
       </section>
+
+      <nav className="flex items-center justify-between border-t border-gray-200 px-4 sm:px-0">
+        <div className="-mt-px flex w-0 flex-1">
+          <button
+            onClick={() => setCurrentPage(currentPage - 1)}
+            disabled={currentPage === 1}
+            className="inline-flex items-center border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+          >
+            <ArrowLongLeftIcon aria-hidden="true" className="mr-3 h-5 w-5 text-gray-400" />
+            Previous
+          </button>
+        </div>
+        <div className="hidden md:-mt-px md:flex">
+          {[...Array(totalPages)].map((_, i) => (
+            <button
+              key={i + 1}
+              onClick={() => setCurrentPage(i + 1)}
+              className={`inline-flex items-center border-t-2 ${
+                currentPage === i + 1 ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+              } px-4 pt-4 text-sm font-medium`}
+            >
+              {i + 1}
+            </button>
+          ))}
+        </div>
+        <div className="-mt-px flex w-0 flex-1 justify-end">
+          <button
+            onClick={() => setCurrentPage(currentPage + 1)}
+            disabled={currentPage === totalPages}
+            className="inline-flex items-center border-t-2 border-transparent pl-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+          >
+            Next
+            <ArrowLongRightIcon aria-hidden="true" className="ml-3 h-5 w-5 text-gray-400" />
+          </button>
+        </div>
+      </nav>
 
       {isAddModalVisible && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-50 backdrop-blur-sm">
@@ -322,25 +395,8 @@ const Pautan = () => {
           </div>
         </div>
       )}
-
-      {/* <section className="flex justify-end mt-5 max-w-[900px]">
-        <button className="px-4 py-1 font-bold text-white bg-[#FF5436] rounded-full shadow-custom" onClick={handleSave}>
-          Save
-        </button>
-      </section> */}
     </>
   );
 };
 
 export default Pautan;
-
-
-
-
-
-
-
-
-
-
-
