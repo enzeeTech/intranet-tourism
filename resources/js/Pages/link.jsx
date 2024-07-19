2463a1f864f9579e7d0ee1b652576d7eb0c70c1a
Mohamad Safwan <mohamadsafwan.1905@gmail.com>
2024-07-19 18:31:29 +0800

    new update for button Manage Pautan

--- resources/js/Pages/link.jsx
+++ resources/js/Pages/link.jsx
@@ -1,25 +1,32 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PageTitle from '../Components/Reusable/PageTitle';
-import SettingNavigation from '@/Components/Settings/SettingsComponent';
 import Example from '@/Layouts/DashboardLayoutNew';
 import Pautan from '@/Components/Settings/LinkComponent';
 
-
-
 const Settings = () => {
-
+  const handleNavigation = () => {
+    window.location.href = '../settings';
+  };
 
   return (
     <Example>
-        <main className="">
-            <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
-                <div>
-                <PageTitle title="Pautan" />
-                <hr className="file-directory-underline" />
-                <Pautan />
-                </div>
+      <main className="">
+        <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
+          <div>
+            <div className="flex items-center justify-between mb-4">
+              <PageTitle title="Pautan" />
+              <button 
+                className="px-4 py-1 font-bold text-white bg-blue-500 rounded-full"
+                onClick={handleNavigation}
+              >
+                Manage Pautan
+              </button>
             </div>
-        </main>
+            <hr className="file-directory-underline" />
+            <Pautan />
+          </div>
+        </div>
+      </main>
     </Example>
   );
 };
