8f36aec33d67edda325bec43afbcc9360a10cf6a
Dilshan Jayasinghe <dilshanj420@gmail.com>
2024-07-21 15:42:12 +0800

    fix: corrected ordering page logic to load efficiently

--- resources/js/Components/Reusable/DropdownStaffDirectory.jsx
+++ resources/js/Components/Reusable/DropdownStaffDirectory.jsx
@@ -122,7 +122,7 @@ const DepartmentDropdown = ({ departments, onSelectDepartment, staffMembers }) =
         className="flex items-center text-sm px-4 py-2.5 bg-blue-500 text-white rounded-full hover:bg-blue-700 mt-1" 
         onClick={toggleAddMemberPopup}
         >
-          <img src="/assets/plus.svg" alt="Plus icon" className="h-3 w-3 mr-2" />
+          <img src="/assets/plus.svg" alt="Plus icon" className="w-3 h-3 mr-2" />
           Member
         </button>
       )}
@@ -148,7 +148,7 @@ const DepartmentDropdown = ({ departments, onSelectDepartment, staffMembers }) =
           </button>
           <hr className="popup-divider" />
           <a
-            href={`/ordering?staffMembers=${encodeURIComponent(JSON.stringify(staffMembers))}`}
+            href={`/ordering?departmentId=${selectedDepartment.id}`}
             className="popup-button hover:bg-gray-100"
           >
             Ordering
