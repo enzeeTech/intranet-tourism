cf42e565abffe2c5bd97f97230e9879d44900aef
Freezeyy <fareezhishamuddin@gmail.com>
2024-07-30 11:59:37 +0800

    [WIP] calendar_of_event

--- resources/js/Pages/Calendar.jsx
+++ resources/js/Pages/Calendar.jsx
@@ -7,6 +7,7 @@ import interactionPlugin from '@fullcalendar/interaction';
 import searchIcon from '../../../public/assets/search.png';
 import searchButton from '../../../public/assets/searchButton.png';
 import printIcon from '../../../public/assets/PrintPDF.svg';
+import pencilIcon from '../../../public/assets/EditIcon.svg'
 import * as bootstrap from "bootstrap";
 import "./Calendar/index.css";
 import Example from '@/Layouts/DashboardLayoutNew';
@@ -195,6 +196,22 @@ function Calendar() {
         }
     };
     
+    const handleEditClick = (event) => {
+        // Logic to handle the edit action, e.g., opening a modal with event details for editing
+        console.log('Editing event:', event);
+        // You can set state to show an edit modal here
+        setEventData({
+            title: event.title,
+            venue: event.venue,
+            date: event.start,
+            startTime: event.startTime,
+            endTime: event.endTime,
+            color: event.color,
+            url: event.url || '',
+        });
+        setIsModalOpen(true);
+    };
+    
     
 
     return (
@@ -205,18 +222,18 @@ function Calendar() {
                 <div className="flex justify-center mt-3 mb-4">
 
                     <div className="flex justify-center rounded-full" style={{ border: '2px solid #E4E4E4', width: '90%' }}>
-                        <div className="flex items-center" style={{ width: '100%' }}>
+                        <div className="flex items-center">
                             <span className="flex items-center justify-center p-2 bg-white rounded-l-full">
                                 <img src={searchIcon} alt="Search" className="w-8 h-8 ml-4" />
                             </span>
-                            <input 
+                            <input
                                 type="search"
                                 className="flex-grow px-4 py-2 border-none input-no-outline"
                                 placeholder="Search for events"
                                 aria-label="Search"
                                 value={searchTerm}
                                 onChange={(e) => setSearchTerm(e.target.value)}
-                                style={{ outline: 'none' }}
+                                style={{ outline: 'none', marginRight: '850px' }}
                             />
                             {/* <button
                                 onClick={filterEvents}
@@ -224,22 +241,15 @@ function Calendar() {
                                 <img src={searchButton} alt="Find Events" className="w-30 h-11" />
                             </button> */}
                         </div>
-                    
                     </div>
-                    
-                    <button
-                        onClick={filterEvents}
-                        className="flex items-center justify-center px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white rounded-full">
-                        Search
-                    </button>
                     <button
                         onClick={handlePrint}
-                        className="flex items-center justify-center bg-red-500 hover:bg-red-700 px-4 rounded-full">
+                        className="flex items-center justify-center bg-red-500 hover:bg-red-700 px-4 rounded-full ml-3">
                         <img src={printIcon} alt="Print" className="w-6 h-6" />
                     </button>
                 </div>
 
-                <div className='flex justify-end -mt-5' >
+                <div className='flex justify-end mb-3' >
                     <button
                         onClick={() => setIsModalOpen(true)}
                         className="flex items-center px-4 py-2 text-white bg-blue-500 hover:bg-blue-700 rounded-full">
@@ -300,6 +310,37 @@ function Calendar() {
                             html: true,
                         });
                     }}                    
+                    // eventContent={(eventInfo) => {
+                    //     return (
+                    //         <div
+                    //             style={{
+                    //                 backgroundColor: eventInfo.event.backgroundColor,
+                    //                 padding: '0 5px',
+                    //                 borderRadius: '2px',
+                    //                 display: 'flex',
+                    //                 alignItems: 'center',
+                    //                 height: '100%',
+                    //                 width: '100%',
+                    //                 whiteSpace: 'nowrap', // Ensure the title doesn't wrap
+                    //                 overflow: 'hidden', // Hide overflow text
+                    //                 textOverflow: 'ellipsis', // Add ellipsis for overflow text
+                    //             }}
+                    //             className="fc-event-title"
+                    //         >
+                    //             <div
+                    //                 style={{
+                    //                     borderLeft: `5px solid ${eventInfo.event.backgroundColor}`,
+                    //                     height: '100%',
+                    //                     opacity: '50%',
+                    //                 }}
+                    //             />
+                    //             <span className="event-title" style={{ color: 'white' }}>
+                    //                 {eventInfo.event.title}
+                    //             </span>
+                    //         </div>
+                    //     );
+                    // }}
+
                     eventContent={(eventInfo) => {
                         return (
                             <div
@@ -324,127 +365,119 @@ function Calendar() {
                                         opacity: '50%',
                                     }}
                                 />
-                                <span className="event-title" style={{ color: 'white' }}>
+                                <span className="event-title" style={{ color: 'white', flexGrow: 1 }}>
                                     {eventInfo.event.title}
                                 </span>
+                                <img
+                                    src={pencilIcon}
+                                    alt="Edit"
+                                    className="w-4 h-4 ml-2 cursor-pointer inline-block"
+                                    onClick={(e) => {
+                                        e.stopPropagation(); // Prevent triggering other event handlers
+                                        e.preventDefault(); // Prevent default behavior
+                                        handleEditClick(eventInfo.event);
+                                    }}
+                                />
                             </div>
                         );
                     }}
                 />
                 <div className='pb-10'></div>
                 {isModalOpen && (
-                    <div
-                        style={{
-                            position: 'fixed',
-                            top: '50%',
-                            left: '50%',
-                            transform: 'translate(-50%, -50%)',
-                            backgroundColor: 'white',
-                            border: '1px solid #ccc',
-                            padding: '20px',
-                            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-                            zIndex: '1050',
-                        }}
-                    >
-                        <button
-                            onClick={closeModal}
-                            style={{
-                                position: 'absolute',
-                                top: '10px',
-                                right: '10px',
-                                border: 'none',
-                                background: 'none',
-                                cursor: 'pointer',
-                            }}
-                        >
-                            X
-                        </button>
-                        <form onSubmit={handleSubmit}>
+                    <div className="modal-container">
+                    <button className="modal-close-button" onClick={closeModal}>
+                      X
+                    </button>
+                    <form onSubmit={handleSubmit}>
+                      <input
+                        type="text"
+                        name="title"
+                        value={eventData.title}
+                        onChange={handleChange}
+                        className="form-control"
+                        placeholder="Event Title"
+                        required
+                      />
+                      <input
+                        type="text"
+                        name="venue"
+                        value={eventData.venue}
+                        onChange={handleChange}
+                        className="form-control"
+                        placeholder="Venue"
+                        required
+                      />
+                      <input
+                        type="date"
+                        name="date"
+                        value={eventData.date}
+                        onChange={handleChange}
+                        className="form-control"
+                        placeholder="Date"
+                        required
+                      />
+                      <input
+                        type="time"
+                        name="startTime"
+                        value={eventData.startTime}
+                        onChange={handleChange}
+                        className="form-control"
+                        placeholder="Start Time"
+                        required
+                      />
+                      <input
+                        type="time"
+                        name="endTime"
+                        value={eventData.endTime}
+                        onChange={handleChange}
+                        className="form-control"
+                        placeholder="End Time"
+                        required
+                      />
+                        <div className="form-group">
+                            <label>
                             <input
-                                type="text"
-                                name="title"
-                                value={eventData.title}
-                                onChange={handleChange}
-                                className="form-control"
-                                placeholder="Event Title"
-                                required
+                                type="checkbox"
+                                className='mr-2'
+                                name="includeUrl"
+                                checked={includeUrl}
+                                onChange={(e) => setIncludeUrl(e.target.checked)}
                             />
+                            Include URL
+                            </label>
+                            {includeUrl && (
                             <input
-                                type="text"
-                                name="venue"
-                                value={eventData.venue}
+                                type="url"
+                                className="form-control mt-2"
+                                name="url"
+                                value={eventData.url}
                                 onChange={handleChange}
-                                className="form-control"
-                                placeholder="Venue"
-                                required
+                                placeholder="Enter event URL"
                             />
-                            <input
-                                type="date"
-                                name="date"
-                                value={eventData.date}
-                                onChange={handleChange}
-                                className="form-control"
-                                placeholder="Date"
-                                required
-                            />
-                            <input
-                                type="time"
-                                name="startTime"
-                                value={eventData.startTime}
-                                onChange={handleChange}
-                                className="form-control"
-                                placeholder="Start Time"
-                                required
-                            />
-                            <input
-                                type="time"
-                                name="endTime"
-                                value={eventData.endTime}
-                                onChange={handleChange}
-                                className="form-control"
-                                placeholder="End Time"
-                                required
-                            />
-                            <div className="form-group">
-                                <label>
-                                    <input
-                                        type="checkbox"
-                                        name="includeUrl"
-                                        checked={includeUrl}
-                                        onChange={(e) => setIncludeUrl(e.target.checked)}
-                                    />
-                                    Include URL
-                                </label>
-                                {includeUrl && (
-                                    <input
-                                        type="url"
-                                        className="form-control"
-                                        name="url"
-                                        value={eventData.url}
-                                        onChange={handleChange}
-                                        placeholder="Enter event URL"
-                                    />
-                                )}
-                            </div>
-                            <select
-                                name="color"
-                                value={eventData.color}
-                                onChange={handleChange}
-                                className="form-control"
-                                required
-                            >
-                                <option value="red">Red</option>
-                                <option value="blue">Blue</option>
-                                <option value="green">Green</option>
-                                <option value="orange">Orange</option>
-                                <option value="purple">Purple</option>
-                                <option value="DeepPink">Pink</option>
-                                <option value="black">Black</option>
-                                <option value="gray">Gray</option>
-                            </select>
-                            <button type="submit">Confirm</button>
-                        </form>
-                    </div>
+                            )}
+                        </div>
+                      <select
+                        name="color"
+                        value={eventData.color}
+                        onChange={handleChange}
+                        className="form-control"
+                        required
+                      >
+                        <option value="red">Red</option>
+                        <option value="blue">Blue</option>
+                        <option value="green">Green</option>
+                        <option value="orange">Orange</option>
+                        <option value="purple">Purple</option>
+                        <option value="DeepPink">Pink</option>
+                        <option value="black">Black</option>
+                        <option value="gray">Gray</option>
+                      </select>
+                      <button type="submit" className="modal-submit-button">
+                        Confirm
+                      </button>
+                    </form>
+                  </div>
+                  
                 )}
                 {isPrintModalOpen && (
                     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
