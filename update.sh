a66b2d8f4f08d50cac2a5611fdba4a69b033351f
Benjamin Irfan <benjaminirfan.work@gmail.com>
2024-07-31 20:56:23 +0800

    fixed composer install in update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -e
+
 php artisan optimize:clear &&
-    php artisan config:cache &&
-    composer install &&
-    php artisan storage:link &&
-    npm install &&
-    npm run build &&
-    sudo systemctl restart nginx &&
-    sudo systemctl restart artisan-queue &&
-    sudo systemctl restart artisan-reverb
+php artisan config:cache &&
+composer install --no-dev --prefer-dist &&
+php artisan storage:link &&
+npm install &&
+npm run build &&
+sudo systemctl restart nginx &&
+sudo systemctl restart artisan-queue &&
+sudo systemctl restart artisan-reverb
