c0a95db64dca1dacea951c9e1470d5f2bb7a16d8
huzaifah23 <132256337+huzaifah23@users.noreply.github.com>
2024-08-01 18:53:13 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,10 @@
 php artisan optimize:clear &&
     php artisan config:cache &&
     composer install --no-dev --prefer-dist &&
+    chown -R www-data:www-data /var/www/intranet-tourism/storage
+    chown -R www-data:www-data /var/www/intranet-tourism/bootstrap/cache
+    chmod -R 775 /var/www/intranet-tourism/storage 
+    chmod -R 775 /var/www/intranet-tourism/bootstrap/cache
     php artisan storage:link &&
     npm install &&
     npm run build &&
