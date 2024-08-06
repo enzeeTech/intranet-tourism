db54b4ca46c03373ff3d1b82289df45e27c1a049
Dilshan Jayasinghe <dilshan.sj@socio.com.my>
2024-08-07 00:13:40 +0800

    style: fix background color across all modules

--- resources/js/Pages/link.jsx
+++ resources/js/Pages/link.jsx
@@ -16,7 +16,7 @@ const Settings = () => {
   //           <div className="flex items-center justify-between mb-4">
   //             <PageTitle title="Pautan" />
   //             <button 
-  //               className="px-4 py-2 font-bold text-white bg-blue-500 hover:bg-blue-700 rounded-full"
+  //               className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700"
   //               onClick={handleNavigation}
   //             >
   //               Manage Links
@@ -32,13 +32,13 @@ const Settings = () => {
 
   return (
     <Example>
-      <main className="">
+      <main className="min-h-screen bg-gray-100">
         <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6 ">
             <div className="flex items-start justify-between mb-2 border-b border-gray-200">
               <h2 className="mb-3 text-3xl font-bold">Pautan</h2>
               <div className="flex items-center justify-between mb-4">
                   <button 
-                    className="px-4 py-2 font-bold text-white bg-blue-500 hover:bg-blue-700 rounded-full"
+                    className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700"
                     onClick={handleNavigation}
                   >
                     Manage Links
