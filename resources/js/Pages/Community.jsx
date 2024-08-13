7ae10b517d2180cb6b4fcf0842ba4e5679527de6
azief <aziefzul@gmail.com>
2024-08-13 23:37:48 +0800

    create new grid style for dept and community because acap want to so im the one who needs to do

--- resources/js/Pages/Community.jsx
+++ resources/js/Pages/Community.jsx
@@ -72,7 +72,7 @@ const Community = () => {
             onSelectDepartment={() => {}}
             onCreateDepartment={handleNewDepartment}
           />
-          <div className="staff-member-grid-container max-w-[1230px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-0 sm:p-4 md:p-6 lg:p-8">
+          <div className="taff-member-grid-container max-w-[1230px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 py-2 sm:py-4 md:py-6 lg:py-8">
             {isLoading ? (
               <div className="mt-20 ml-32 loading-spinner"></div>
             ) : filteredDepartments.length === 0 ? (
