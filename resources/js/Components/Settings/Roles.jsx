13516d942d9fdb5cd0dfad0aab477d32e3c3d641
Afiqenzee <afiq.hasif@enzee.io>
2024-08-14 17:05:58 +0800

    new update for setting/roles

--- resources/js/Components/Settings/Roles.jsx
+++ resources/js/Components/Settings/Roles.jsx
@@ -58,58 +58,50 @@ export default function Roles() {
     };
 
     return (
-        <div className="px-4 sm:px-6 lg:px-8">
-            <div className="sm:flex sm:items-center">
-                <div className="file-search-bar-title mr-2 mt-5">
-                <h2 className="text-xl font-bold">Roles</h2>
-                    <p className="text-sm text-gray-600 mt-1">
-                        Manage and assign roles to team members
-                    </p>
-                </div>
-
-                <div className="sm:flex-auto">
-                    {/* <h1 className="text-base font-semibold leading-6 text-gray-900">Users</h1> */}
-                    {/* <p className="mt-2 text-sm text-gray-700">
-            A list of all the users in your account including their name, title, email, and role.
-          </p> */}
-                </div>
-
-                <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+        <div className="flow-root">
+            <div className="container p-8 mx-auto">
+                <div className="flex items-center justify-between mb-8">
+                    <div>
+                        <h1 className="text-3xl font-bold text-gray-900">
+                            Roles
+                        </h1>
+                        <p className="text-gray-600">
+                            Manage the role here to Admin or Super Admin.
+                        </p>
+                    </div>
                     <button
-                        type="button"
-                        className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-bold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        onClick={() => setIsPopupOpen(true)}
+                        className="px-4 py-2 text-white bg-blue-600 rounded hover:bg-blue-700 rounded-full"
                     >
-                        Assign as Admin
+                        Assign As Admin
                     </button>
                 </div>
-            </div>
-            <div className="mt-8 flow-root">
-                <div className="box-container -mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="box-container -mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-1">
                     <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
                         <table className="min-w-full divide-y divide-gray-300">
                             <thead>
                                 <tr>
                                     <th
                                         scope="col"
-                                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-500 sm:pl-0"
                                     >
                                         Name
                                     </th>
                                     <th
                                         scope="col"
-                                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-500"
                                     >
                                         Title
                                     </th>
                                     <th
                                         scope="col"
-                                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-500"
                                     >
                                         Status
                                     </th>
                                     <th
                                         scope="col"
-                                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-500"
                                         style={{ width: "12rem" }}
                                     >
                                         Role
