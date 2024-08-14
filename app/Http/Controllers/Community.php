ed7ac02b7febd902331e18da16ff8033bfcfa620
fqhaif01 <Fqhasif01@gmail.com>
2024-08-14 09:53:56 +0800

    pull community

---
+++ app/Http/Controllers/Community.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+
+class Community extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Community', ['id' => auth()->id()]);
+    }
+    public function renderinner()
+    {
+        return Inertia::render('CommunityInner', ['id' => auth()->id()]);
+    }
+}
