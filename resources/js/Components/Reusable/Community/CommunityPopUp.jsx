590f0f63640e0a5a3d9497019784f1e532ee1bf9
Benjamin Irfan <benjaminirfan.work@gmail.com>
2024-08-15 15:55:17 +0800

    fixed the community card popup styling

--- resources/js/Components/Reusable/Community/CommunityPopUp.jsx
+++ resources/js/Components/Reusable/Community/CommunityPopUp.jsx
@@ -17,12 +17,12 @@ const PopupMenu = ({ onAssign, selectedDepartmentId, onClose }) => {
   }, [onClose]);
 
   return (
-    <div ref={popupRef} className="absolute right-0 z-50 bg-white border shadow-lg w-[190px] rounded-xl -mt-3">
+    <div ref={popupRef} className="absolute right-0 z-50 bg-white border shadow-lg w-[190px] rounded-xl -mt-20">
       <button onClick={onAssign} className="flex items-center w-full px-4 py-2 text-sm font-extrabold text-gray-700 hover:bg-gray-100 hover:rounded-t-xl">
-        Reporting Structure
+        Archive
       </button>
       <button onClick={onAssign} className="flex items-center w-full px-4 py-2 text-sm font-extrabold text-gray-700 hover:bg-gray-100 hover:rounded-t-xl">
-        Reporting Structure
+        Delete
       </button>
     </div>
   );
