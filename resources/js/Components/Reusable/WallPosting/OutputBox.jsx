57b49b7fc60fab0745532543f2ab2b4deb88a4ff
Afiqenzee <afiq.hasif@enzee.io>
2024-08-26 16:01:59 +0800

    OutputBox Wallposting

--- resources/js/Components/Reusable/WallPosting/OutputBox.jsx
+++ resources/js/Components/Reusable/WallPosting/OutputBox.jsx
@@ -133,14 +133,11 @@ async function fetchData() {
           if (accessibility.accessable_type === accessibility.accessable_type) {
             const departmentResponse = await fetch(`/api/department/departments/${accessibility.accessable_id}`);
             const departmentData = await departmentResponse.json();
-            console.log("HAHAHA", departmentData.data.name);
-            
             return departmentData.data.name;
           }
           return null;
         }));
         post.departmentNames = departmentNames.filter(name => name !== null).join(', ');
-        console.log("HAHAHA", post.departmentNames);
       } else {
         post.departmentNames = null;
       }
@@ -340,8 +337,8 @@ console.log("FINAL", finalPosts);
     // Function to handle liking a post
     const handleLike = async (postId) => {
       try {
-        const response = await fetch(`/api/posts/posts/${postId}/like`, {
-          method: 'POST',
+        const response = await fetch(`/api/posts/post/like/${postId}`, {
+          method: 'PUT',
           headers: { 'X-CSRF-Token': csrfToken },
         });
   
@@ -362,8 +359,8 @@ console.log("FINAL", finalPosts);
     // Function to handle unliking a post
     const handleUnlike = async (postId) => {
       try {
-        const response = await fetch(`/api/posts/posts/${postId}/unlike`, {
-          method: 'POST',
+        const response = await fetch(`/api/posts/post/unlike/${postId}`, {
+          method: 'PUT',
           headers: { 'X-CSRF-Token': csrfToken },
         });
   
@@ -395,7 +392,7 @@ console.log("FINAL", finalPosts);
 
   const renderContentWithTags = (content) => {
     // Regex to match tags (e.g., @username or @FirstName LastName)
-    const tagRegex = /@\w+(?:\s\w)*\b/g;
+    const tagRegex = /@\w+(?:\s\w+)*\b/g;
     // Regex to match URLs starting with https
     const urlRegex = /https:\/\/[^\s]+/g;
 
@@ -581,7 +578,7 @@ console.log("FINAL", finalPosts);
           }
 
           return (
-            <div key={post.id}>
+            <div className="w-full" key={post.id}>
               {/* Conditional Rendering for Announcement */}
               {post.type === 'announcement' && (
                 <div className="mt-10 py-2 px-6 border rounded-2xl border-2 shadow-xl w-full lg:w-[610px] md:w-[610px] sm:w-[610px] relative pb-16 bg-[#FF5437]">
@@ -594,7 +591,7 @@ console.log("FINAL", finalPosts);
                 </div>
               )}
 
-               {/* Birthday Post on Public Wall */}
+               {/* Birthday Post */}
                {post.type === 'birthday' && (
                  <article className={`${post.type === 'announcement' ? '-mt-16' : 'mt-10'} p-4 border rounded-2xl bg-white border-2 shadow-xl w-full lg:w-[610px] md:w-[610px] sm:w-[610px] relative`}>
                    <header className="flex px-px w-full max-md:flex-wrap max-md:max-w-full ">
@@ -672,187 +669,43 @@ console.log("FINAL", finalPosts);
                    </header>
 
                    {!post.attachments || post.attachments.length === 0 ? (
-                      // Render this block if there are no attachments
-                      <>
-                        <div>{post.content}</div>
-                        <p className="mt-3.5 text-xs font-semibold leading-6 text-blue-500 max-md:max-w-full">
-                          {post.mentions?.replace(/[\[\]"]/g, '') || ''}
-                        </p>
-                      </>
-                    ) : (
-                      // Render this block if there are attachments
-                      <>
-                        <p className="mt-3.5 text-xs font-semibold leading-6 text-blue-500 max-md:max-w-full">
-                          {post.mentions?.replace(/[\[\]"]/g, '') || ''}
-                        </p>
-                        <div className="relative flex flex-wrap gap-2 mt-4">
-                          {post.attachments.map((attachment, idx) => (
-                            <div key={idx} className="relative w-full">
-                              <img
-                                src={`/storage/${attachment.path}`}
-                                alt={`Attachment ${idx + 1}`}
-                                className="rounded-xl w-full h-auto object-cover"
-                                style={{ maxHeight: '300px' }} // Allowing the image to take up more vertical space
-                              />
-                              {idx === Math.floor(post.attachments.length / 2) && (
-                                <div className="absolute inset-0 flex justify-center items-center p-4">
-                                  <span
-                                    className="text-5xl font-black text-center text-white text-opacity-90 bg-black bg-opacity-50 rounded-lg"
-                                    style={{ maxWidth: '90%', overflowWrap: 'break-word', wordWrap: 'break-word' }}
-                                  >
-                                    {post.content}
-                                  </span>
-                                </div>
-                              )}
-                            </div>
-                          ))}
-                        </div>
-                      </>
-                    )}
-
-                   <div className="flex items-center gap-4 mt-2">
-                     <div className="flex items-center gap-2">
-                       {isPostLikedByUser(post) ? (
-                         <img
-                           src="/assets/Like.svg"
-                           alt="Unlike"
-                           className="w-5 h-5 cursor-pointer"
-                           onClick={() => handleUnlike(post.id)}
-                         />
-                       ) : (
-                         <img
-                           src="/assets/likeforposting.svg"
-                           alt="Like"
-                           className="w-5 h-5 cursor-pointer"
-                           onClick={() => handleLike(post.id)}
-                         />
-                       )}
-                       {likesCount > 0 && <span className="text-sm font-medium">{likesCount}</span>}
-                     </div>
-                     <img src="/assets/commentforposting.svg" alt="Comment" className="w-6 h-6 cursor-pointer" onClick={() => openCommentPopup(post)} />
-                   </div>
-                 </article>
-                )}
-
-                {/* Birthday Post from Department */}
-               {post.type === 'Admin Wish' && (
-                 <article className={`${post.type === 'announcement' ? '-mt-16' : 'mt-10'} p-4 border rounded-2xl bg-white border-2 shadow-xl w-[610px] relative`}>
-                   <header className="flex px-px w-full max-md:flex-wrap max-md:max-w-full">
-                     <div className="flex gap-1 mt-2"></div>
-                     <div className="flex justify-between items-start px-1 w-full mb-4 p-2 -ml-2 -mt-3">
-                       <div className="flex gap-5 justify-between w-full max-md:flex-wrap max-md:max-w-full">
-                         <div className="flex gap-1.5 -mt-1">
-                           {/* <img 
-                             loading="lazy" 
-                             src={
-                               post.userProfile.profile?.image 
-                                   ? (
-                                       post.userProfile.profile.image === '/assets/dummyStaffPlaceHolder.jpg'
-                                           ? post.userProfile.profile.image
-                                           : post.userProfile.profile.image.startsWith('avatar/')
-                                               ? `/storage/${post.userProfile.profile.image}`
-                                               : `/avatar/${post.userProfile.profile.image}`
-                                   )
-                                   : `https://ui-avatars.com/api/?background=0D8ABC&color=fff&name=${encodeURIComponent(post.user.name)}&rounded=true`
-                             } 
-                             alt={post.user.name} 
-                             className="shrink-0 aspect-square w-[53px] rounded-image" 
-                           /> */}
-                           <div className="flex flex-col my-auto">
-                             {/* <div className="text-base font-semibold text-neutral-800">{post.user.name}</div> */}
-                             <div className="text-base font-semibold text-neutral-800">{post.departmentNames}: <br></br>Wishing you a...</div>
-
-                             {/* <div className="text-base font-semibold text-neutral-800">Admin Wish u A Very Happy Bday!!!</div> */}
-
-                             <time className="mt-1 text-xs text-neutral-800 text-opacity-50">{formatTimeAgo(post.created_at)}</time>
-                           </div>
-                         </div>
-                         <div className="flex items-center gap-2">
-                           {/* <span className="text-sm font-semibold text-neutral-800 bg-gray-200 rounded-md px-2 py-1 -mt-5">
-                             {post.accessibilities?.map((accessibility, index) => (
-                               <span key={index}>
-                                 {accessibility.accessable_type}{": "}
-                               </span>
-                             ))}
-                             {post.departmentNames ? post.departmentNames : post.type}
-                           </span> */}
-                           <img 
-                             loading="lazy" 
-                             src="/assets/wallpost-dotbutton.svg" 
-                             alt="Options" 
-                             className="shrink-0 my-auto aspect-[1.23] fill-red-500 w-6 cursor-pointer mt-1" 
-                             onClick={() => togglePopup(index)} 
-                           />
-                         </div>
-                       </div>
-                     </div>
-                     {isPopupOpen[index] && (
-                       <div className="absolute bg-white border-2 rounded-xl p-1 shadow-lg mt-6 right-0 w-[160px] h-auto z-10">
-                         <p 
-                           className="cursor-pointer flex flex-row hover:bg-blue-100 rounded-xl p-2" 
-                           onClick={() => handleEdit(post)}
-                         >
-                           <img className="w-6 h-6" src="/assets/EditIcon.svg" alt="Edit" />
-                           Edit
-                         </p>
-                         <div className="font-extrabold text-neutral-800 mb-1 mt-1 border-b-2 border-neutral-300"></div>
-                         <p 
-                           className="cursor-pointer flex flex-row hover:bg-blue-100 rounded-xl p-2" 
-                           onClick={() => confirmDelete(post.id)}
-                         >
-                           <img className="w-6 h-6" src="/assets/DeleteIcon.svg" alt="Delete" />
-                           Delete
-                         </p>
-                         <div className="font-extrabold text-neutral-800 mb-2 mt-1 border-b-2 border-neutral-300"></div>
-                         <p 
-                           className="cursor-pointer flex flex-row hover:bg-blue-100 rounded-xl p-2" 
-                           onClick={() => handleAnnouncement(post)}
-                         >
-                           <img className="w-6 h-6" src="/assets/AnnounceIcon.svg" alt="Announcement" />
-                           Announcement
-                         </p>
-                       </div>
-                     )}
-                   </header>
-
-                   {!post.attachments || post.attachments.length === 0 ? (
-                      // Render this block if there are no attachments
-                      <>
-                        <div>{post.content}</div>
-                        <p className="mt-3.5 text-xs font-semibold leading-6 text-blue-500 max-md:max-w-full">
-                          {post.mentions?.replace(/[\[\]"]/g, '') || ''}
-                        </p>
-                      </>
-                    ) : (
-                      // Render this block if there are attachments
-                      <>
-                        <p className="mt-3.5 text-xs font-semibold leading-6 text-blue-500 max-md:max-w-full">
-                          {post.mentions?.replace(/[\[\]"]/g, '') || ''}
-                        </p>
-                        <div className="relative flex flex-wrap gap-2 mt-4">
-                          {post.attachments.map((attachment, idx) => (
-                            <div key={idx} className="relative w-full">
-                              <img
-                                src={`/storage/${attachment.path}`}
-                                alt={`Attachment ${idx + 1}`}
-                                className="rounded-xl w-full h-auto object-cover"
-                                style={{ maxHeight: '300px' }} // Allowing the image to take up more vertical space
-                              />
-                              {idx === Math.floor(post.attachments.length / 2) && (
-                                <div className="absolute inset-0 flex justify-center items-center p-4">
-                                  <span
-                                    className="text-5xl font-black text-center text-white text-opacity-90 bg-black bg-opacity-50 rounded-lg"
-                                    style={{ maxWidth: '90%', overflowWrap: 'break-word', wordWrap: 'break-word' }}
-                                  >
-                                    {post.content}
-                                  </span>
-                                </div>
-                              )}
-                            </div>
-                          ))}
-                        </div>
-                      </>
-                    )}
+  // Render this block if there are no attachments
+  <>
+    <div>{post.content}</div>
+    <p className="mt-3.5 text-xs font-semibold leading-6 text-blue-500 max-md:max-w-full">
+      {post.mentions?.replace(/[\[\]"]/g, '') || ''}
+    </p>
+  </>
+) : (
+  // Render this block if there are attachments
+  <>
+    <p className="mt-3.5 text-xs font-semibold leading-6 text-blue-500 max-md:max-w-full">
+      {post.mentions?.replace(/[\[\]"]/g, '') || ''}
+    </p>
+    <div className="relative flex flex-wrap gap-2 mt-4">
+      {post.attachments.map((attachment, idx) => (
+        <div key={idx} className="relative w-full">
+          <img
+            src={`/storage/${attachment.path}`}
+            alt={`Attachment ${idx + 1}`}
+            className="rounded-xl w-full h-auto object-cover"
+            style={{ maxHeight: '300px' }} // Allowing the image to take up more vertical space
+          />
+          {idx === Math.floor(post.attachments.length / 2) && (
+            <div className="absolute inset-0 flex justify-center items-center p-4">
+              <span
+                className="text-5xl font-black text-center text-white text-opacity-90 bg-black bg-opacity-50 rounded-lg"
+                style={{ maxWidth: '90%', overflowWrap: 'break-word', wordWrap: 'break-word' }}
+              >
+                {post.content}
+              </span>
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  </>
+)}
 
                    <div className="flex items-center gap-4 mt-2">
                      <div className="flex items-center gap-2">
@@ -962,18 +815,13 @@ console.log("FINAL", finalPosts);
                     {renderContentWithTags(post.content)}
                 </article>
 
-                {post.tag?.length > 0 && (
                 <p className="mt-3.5 text-xs font-semibold leading-6 text-blue-500 max-md:max-w-full">
-                  Tagged Album: {post.tag?.replace(/[\[\]"]/g, '') || ''}
+                  {/* {post.tag.replace(/[\[\]"]/, '')} */}
+                  {post.tag?.replace(/[\[\]"]/g, '') || ''}
                 </p>
-                )}
-
-                {post.mentions?.length > 0 && (
-                    <p className="mt-3.5 text-xs font-semibold leading-6 text-blue-500 max-md:max-w-full">
-                      Tagged People: {post.mentions.replace(/[\[\]"]/g, '')}
-                    </p>
-                )}
-
+                <p className="mt-3.5 text-xs font-semibold leading-6 text-blue-500 max-md:max-w-full">
+                {post.mentions?.replace(/[\[\]"]/g, '') || ''}
+                </p>
                 <p className="mt-3.5 text-xs font-semibold leading-6 text-blue-500 max-md:max-w-full">
                 {post.event?.replace(/[\[\]"]/g, '') || ''}
                 </p>
