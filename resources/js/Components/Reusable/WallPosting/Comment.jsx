5eeeb83db1d1642cbbc50c36d72ba8c843e88b16
Afiqenzee <afiq.hasif@enzee.io>
2024-08-28 11:03:09 +0800

    Update in comment

--- resources/js/Components/Reusable/WallPosting/Comment.jsx
+++ resources/js/Components/Reusable/WallPosting/Comment.jsx
@@ -347,7 +347,7 @@ const Comment = ({ post, onClose, loggedInUserId }) => {
                         <img
                           src="/assets/likeforposting.svg"
                           alt="Like"
-                          className="w-5 h-5 cursor-pointer"
+                          className="w-5 h-3 mt-2 cursor-pointer"
                           onClick={() => handleLike(comment.id)}
                         />
                       )}
