35390dacdcf102c244466490b1afb485dac64577
Benjamin Irfan <benjaminirfan.work@gmail.com>
2024-09-02 17:34:34 +0800

    edited the popup UI

--- resources/js/Components/Reusable/Community/CommunityPopUp.jsx
+++ resources/js/Components/Reusable/Community/CommunityPopUp.jsx
@@ -54,16 +54,16 @@ const PopupMenu = ({ onArchiveToggle, selectedDepartmentId, onClose, onDelete })
         <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
           <div className="bg-white p-6 rounded-lg shadow-lg">
             <p className="mb-4 text-lg">Are you sure you want to delete this community?</p>
-            <div className="flex justify-end">
+            <div className="flex justify-center space-x-4">
               <button
                 onClick={handleCancelDelete}
-                className="bg-gray-500 text-white px-4 py-2 rounded-md mr-2"
+                className="px-8 py-1 text-white bg-red-500 rounded-full hover:bg-red-700"
               >
                 No
               </button>
               <button
                 onClick={handleConfirmDelete}
-                className="bg-red-500 text-white px-4 py-2 rounded-md"
+                className="px-8 py-1 text-base text-gray-400 bg-white border border-gray-400 rounded-full hover:bg-gray-400 hover:text-white"
               >
                 Yes
               </button>
